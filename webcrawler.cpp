4db4f5e35f7c48da513e1b242d987eb810ce5a71
sun549 <sun549@purdue.edu>
2017-07-08 15:23:51 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -1,14 +1,42 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "webcrawler.h"
 
 // Add your implementation here
+
+char *word;
+char *description;
+
 WebCrawler::WebCrawler(int maxUrls, int nurlRoots, const char **urlRoots)
 {
+	// Allocate space for _urlArray
+	// insert the initiaLURLs
+	// Update _maxUrls, _headURL, and _tailURL
 }
 
 void
 WebCrawler::crawl()
 {
+	while (_headURL < _tailURL) {
+		// Fetch the next URL in _headURL
+		//
+		// increment _headURL
+		// if the document is not text/html
+		// countinue;
+		// Get the first 500 charactoers (at most) of the document without tags.
+		// Add this description to the URL record for this URL
+		//
+		// Find all the hyperlinks of this document
+		// Add them to the _urlArray and _urlToRecord 
+		// if they are not already in the _urlToUrlRecord. Only insert up to _maxURL entries
+		//
+		// For each word in the document without tags, add the index of this URL to
+		// a URLRecordList in the _wordToURLRecordList table if the URL is not already there
+
+	}
 }
 
 void
@@ -19,4 +47,8 @@ WebCrawler::writeURLFile(const char *urlFileName)
 void
 WebCrawler::writeWordFile(const char *wordFileName)
 {
+}
+
+int main() {
+	return 0;
 }
