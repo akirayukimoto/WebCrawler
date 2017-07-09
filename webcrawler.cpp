4d01d10bdacf99e9c7bc68c3315967b751624074
sun549 <sun549@purdue.edu>
2017-07-08 23:43:10 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -15,6 +15,24 @@ WebCrawler::WebCrawler(int maxUrls, int nurlRoots, const char **urlRoots)
 	// Allocate space for _urlArray
 	// insert the initiaLURLs
 	// Update _maxUrls, _headURL, and _tailURL
+	_maxUrls = maxUrls;
+	_headURL = 0;
+	_tailURL = nurlRoots;
+	_urlArray = new URLRecord[maxUrls + nurlRoots];
+
+	for (int i = 0; i < nurlRoots; i++) {
+		//char *url = strdup(urlRoots[i]);
+		int len = strlen(urlRoots[i]);
+		char *url = new char[len];
+		//char *url = new char [strlen(urlRoots[i])];
+		strcpy(url, urlRoots[i]);
+		if (url[len - 1] != '/') url[len - 1] = '/';
+		_urlArray[i]._url = strdup(url);
+		_urlArray[i]._description = NULL;
+	
+	}
+	_urlToUrlRecord = new HashTableTemplate<int>();
+	_urlToUrlRecordList = new HashTableTemplate<URLRecordList*>();
 }
 
 void
@@ -42,11 +60,25 @@ WebCrawler::crawl()
 void
 WebCrawler::writeURLFile(const char *urlFileName)
 {
+	FILE *f = fopen(urlFileName, "r");
+	for (int i = 0; i < _tailURL; i++) {
+		fprintf(f, "%d %s\n",i, _urlArray[i]._url);
+		if (_urlArray[i]._description != NULL) 
+			fprintf(f, "%s\n", _urlArray[i]._description);
+		else fprintf(f, "%s\n", "");
+		fprintf(f, "%s\n", "");
+	}
+	fclose(f);
 }
 
 void
 WebCrawler::writeWordFile(const char *wordFileName)
 {
+	FILE *file = fopen(wordFileName, "w");
+	for (int i = 0; i < _tailURL; i++) {
+		fprintf(file, "%s", _wordToURLRecordList->_buckets[i]._key;
+	}
+	fclose(file);
 }
 
 int main() {
