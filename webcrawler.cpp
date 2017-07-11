1b1b97bb7616eafa013e6a601121fd72b9eb5eb5
sun549 <sun549@purdue.edu>
2017-07-10 23:26:12 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -130,7 +130,7 @@ WebCrawler::insertWord() {
 void
 WebCrawler::writeURLFile(const char *urlFileName)
 {
-	FILE *f = fopen(urlFileName, "a");
+	FILE *f = fopen(urlFileName, "w");
 	for (int i = 0; i < _tailURL; i++) {
 		fprintf(f, "%d %s\n",i, _urlArray[i]._url);
 		if (_urlArray[i]._description != NULL) {
@@ -147,7 +147,7 @@ WebCrawler::writeURLFile(const char *urlFileName)
 void
 WebCrawler::writeWordFile(const char *wordFileName)
 {
-	FILE *file = fopen(wordFileName, "a");
+	FILE *file = fopen(wordFileName, "w");
 
 	HashTableTemplateIterator<URLRecordList *> it(_wordToURLRecordList);
 	const char *key;
