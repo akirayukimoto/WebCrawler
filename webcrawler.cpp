11b17241af014a6c783672cf2df6b2ffa6f0c51b
sun549 <sun549@purdue.edu>
2017-07-10 20:16:55 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -261,9 +261,9 @@ WebCrawler::onAnchorFound(char *url)
 
     {
 
-        char *http = "http://";
+        const char *http = "http://";
 
-        char *https = "https://";
+        const char *https = "https://";
 
         
 
