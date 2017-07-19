af1f9e69c018bfd4157476ac8b504a4a1865631a
sun549 <sun549@purdue.edu>
2017-07-19 13:32:26 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -131,11 +131,14 @@ WebCrawler::writeURLFile(const char *urlFileName)
 	if (f == NULL) return;
 	for (int i = 0; i < _tailURL; i++) {
 		fprintf(f, "%d %s\n",i, _urlArray[i]._url);
-		if (_urlArray[i]._description != NULL) {
+		//if (_urlArray[i]._description != NULL) {
+			if (_urlArray[0] != '\0') {
 			printf("%s\n", _urlArray[i]._description);
 			fprintf(f, "%s\n", _urlArray[i]._description);
-		}
-		else fprintf(f, "%s\n", "No description");
+			}
+			else fprintf(f, "%s\n", "No descriotion");
+		//}
+		//else fprintf(f, "%s\n", "No description");
 		//fprintf(f, "%s\n", "");
 		fprintf(f, "\n");
 	}
